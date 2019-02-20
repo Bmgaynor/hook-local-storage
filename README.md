349a8a042aad58a384bf36090df33474321380c7
Bradley Gaynor <bradleygaynor@northwesternmutual.com>
2019-02-20 15:25:31 -0700

    create-react-catalog

---
+++ README.md
@@ -0,0 +1,11 @@
+## create-react-catalog
+
+https://bmgaynor.gitlab.io/create-react-catalog/#/
+
+
+## Publishing 
+
+```bash
+npm version patch
+git push
+```
---
+++ doczrc.js
@@ -0,0 +1,6 @@
+export default {
+  src: './lib',
+  base: '/use-local-storage/',
+  dest: 'public',
+  public: '/assets' // required to override this since default is the dest 
+}
