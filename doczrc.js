2b942de847ffb439b8cea612864af6074233267c
Bradley Gaynor <bradleygaynor@northwesternmutual.com>
2019-04-02 13:49:12 -0500

    add circleci config

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,4 @@
 export default {
   src: './lib',
-  base: '/use-local-storage/',
-  dest: 'public',
-  public: '/assets' // required to override this since default is the dest 
+  base: '/use-local-storage/'
 }
